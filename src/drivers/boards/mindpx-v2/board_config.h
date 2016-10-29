TheExplorex/OpenMindPX

MindPX: Enable FrkSky telemetry on usart8/ttys6.

src/drivers/boards/mindpx-v2/board_config.h
@@ -292,6 +292,9 @@ __BEGIN_DECLS
 #define GPIO_SBUS_INV		(GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTA|GPIO_PIN10)
 #define INVERT_RC_INPUT(_s)	px4_arch_gpiowrite(GPIO_SBUS_INV, _s);
 
+#define GPIO_FRSKY_INV		(GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTB|GPIO_PIN5)
+#define INVERT_FRSKY(_s)	px4_arch_gpiowrite(GPIO_FRSKY_INV, _s);
+
 /* Power switch controls */
 #define GPIO_SPEKTRUM_PWR_EN	(GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTB|GPIO_PIN12)
 #define POWER_SPEKTRUM(_s)	px4_arch_gpiowrite(GPIO_SPEKTRUM_PWR_EN, _s)

gau914sb/PX4-Voronoi

src/drivers/boards/mindpx-v2/board_config.h
@@ -289,6 +289,9 @@
 #define GPIO_SBUS_INV		(GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTA|GPIO_PIN10)
 #define INVERT_RC_INPUT(_s)	px4_arch_gpiowrite(GPIO_SBUS_INV, _s);
 
+#define GPIO_FRSKY_INV		(GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTB|GPIO_PIN12)
+#define INVERT_FRSKY(_s)	px4_arch_gpiowrite(GPIO_FRSKY_INV, _s);
+
 /* Power switch controls */
 #define GPIO_SPEKTRUM_PWR_EN
 #define POWER_SPEKTRUM(_s)		do { } while (0)