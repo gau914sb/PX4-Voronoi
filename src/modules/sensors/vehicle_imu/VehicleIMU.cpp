sgtan1912/PX4-Autopilot

Add gyro clipping to mirror accel clipping monitoring instances.

src/modules/sensors/vehicle_imu/VehicleIMU.cpp
@@ -394,24 +394,25 @@ bool VehicleIMU::UpdateAccel()
 			_status.accel_clipping[2] += clip_z;
 
 			if (clip_x > 0) {
-				_delta_velocity_clipping |= vehicle_imu_s::CLIPPING_X;
+				_delta_velocity_clipping |= vehicle_imu_s::VELOCITY_CLIPPING_X;
 			}
 
 			if (clip_y > 0) {
-				_delta_velocity_clipping |= vehicle_imu_s::CLIPPING_Y;
+				_delta_velocity_clipping |= vehicle_imu_s::VELOCITY_CLIPPING_Y;
 			}
 
 			if (clip_z > 0) {
-				_delta_velocity_clipping |= vehicle_imu_s::CLIPPING_Z;
+				_delta_velocity_clipping |= vehicle_imu_s::VELOCITY_CLIPPING_Z;
 			}
 
 			_publish_status = true;
 
-			if (_accel_calibration.enabled() && (hrt_elapsed_time(&_last_clipping_notify_time) > 3_s)) {
+			if (_accel_calibration.enabled() && (hrt_elapsed_time(&_last_velocity_clipping_notify_time) > 3_s)) {
 				// start notifying the user periodically if there's significant continuous clipping
-				const uint64_t clipping_total = _status.accel_clipping[0] + _status.accel_clipping[1] + _status.accel_clipping[2];
+				const uint64_t velocity_clipping_total = _status.accel_clipping[0] + _status.accel_clipping[1] +
+						_status.accel_clipping[2];
 
-				if (clipping_total > _last_clipping_notify_total_count + 1000) {
+				if (velocity_clipping_total > _last_velocity_clipping_notify_total_count + 1000) {
 					mavlink_log_critical(&_mavlink_log_pub, "Accel %" PRIu8 " clipping, not safe to fly!\t", _instance);
 					/* EVENT
 					 * @description Land now, and check the vehicle setup.
@@ -419,8 +420,8 @@ bool VehicleIMU::UpdateAccel()
 					 */
 					events::send<uint8_t>(events::ID("vehicle_imu_accel_clipping"), events::Log::Critical,
 							      "Accel {1} clipping, not safe to fly!", _instance);
-					_last_clipping_notify_time = accel.timestamp_sample;
-					_last_clipping_notify_total_count = clipping_total;
+					_last_velocity_clipping_notify_time = accel.timestamp_sample;
+					_last_velocity_clipping_notify_total_count = velocity_clipping_total;
 				}
 			}
 		}
@@ -534,6 +535,52 @@ bool VehicleIMU::UpdateGyro()
 		_gyro_integrator.put(gyro_raw, dt);
 
 		updated = true;
+
+		if (gyro.clip_counter[0] > 0 || gyro.clip_counter[1] > 0 || gyro.clip_counter[2] > 0) {
+			// rotate sensor clip counts into vehicle body frame
+			const Vector3f clipping{_gyro_calibration.rotation() *
+						Vector3f{(float)gyro.clip_counter[0], (float)gyro.clip_counter[1], (float)gyro.clip_counter[2]}};
+
+			// round to get reasonble clip counts per axis (after board rotation)
+			const uint8_t clip_x = roundf(fabsf(clipping(0)));
+			const uint8_t clip_y = roundf(fabsf(clipping(1)));
+			const uint8_t clip_z = roundf(fabsf(clipping(2)));
+
+			_status.gyro_clipping[0] += clip_x;
+			_status.gyro_clipping[1] += clip_y;
+			_status.gyro_clipping[2] += clip_z;
+
+			if (clip_x > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::ANGLE_CLIPPING_ROLL;
+			}
+
+			if (clip_y > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::ANGLE_CLIPPING_PITCH;
+			}
+
+			if (clip_z > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::ANGLE_CLIPPING_YAW;
+			}
+
+			_publish_status = true;
+
+			if (_gyro_calibration.enabled() && (hrt_elapsed_time(&_last_velocity_clipping_notify_time) > 3_s)) {
+				// start notifying the user periodically if there's significant continuous clipping
+				const uint64_t angle_clipping_total = _status.gyro_clipping[0] + _status.gyro_clipping[1] + _status.gyro_clipping[2];
+
+				if (angle_clipping_total > _last_angle_clipping_notify_total_count + 1000) {
+					mavlink_log_critical(&_mavlink_log_pub, "Gyro %" PRIu8 " clipping, not safe to fly!\t", _instance);
+					/* EVENT
+					 * @description Land now, and check the vehicle setup.
+					 * Clipping can lead to fly-aways.
+					 */
+					events::send<uint8_t>(events::ID("vehicle_imu_gyro_clipping"), events::Log::Critical,
+							      "Gyro {1} clipping, not safe to fly!", _instance);
+					_last_angle_clipping_notify_time = gyro.timestamp_sample;
+					_last_angle_clipping_notify_total_count = angle_clipping_total;
+				}
+			}
+		}
 	}
 
 	return updated;

src/modules/sensors/vehicle_imu/VehicleIMU.hpp
@@ -148,10 +148,14 @@ private:
 	float _coning_norm_accum{0};
 	float _coning_norm_accum_total_time_s{0};
 
-	uint8_t _delta_velocity_clipping{0};
+	uint8_t     _delta_angle_clipping{0};
+	hrt_abstime _last_angle_clipping_notify_time{0};
+	uint64_t    _last_angle_clipping_notify_total_count{0};
 
-	hrt_abstime _last_clipping_notify_time{0};
-	uint64_t _last_clipping_notify_total_count{0};
+	uint8_t     _delta_velocity_clipping{0};
+	hrt_abstime _last_velocity_clipping_notify_time{0};
+	uint64_t    _last_velocity_clipping_notify_total_count{0};
+
 	orb_advert_t _mavlink_log_pub{nullptr};
 
 	uint32_t _backup_schedule_timeout_us{20000};

gau914sb/PX4-Voronoi

src/modules/sensors/vehicle_imu/VehicleIMU.cpp
@@ -407,11 +407,11 @@ bool VehicleIMU::UpdateAccel()
 
 			_publish_status = true;
 
-			if (_accel_calibration.enabled() && (hrt_elapsed_time(&_last_clipping_notify_time) > 3_s)) {
+			if (_accel_calibration.enabled() && (hrt_elapsed_time(&_last_accel_clipping_notify_time) > 3_s)) {
 				// start notifying the user periodically if there's significant continuous clipping
 				const uint64_t clipping_total = _status.accel_clipping[0] + _status.accel_clipping[1] + _status.accel_clipping[2];
 
-				if (clipping_total > _last_clipping_notify_total_count + 1000) {
+				if (clipping_total > _last_accel_clipping_notify_total_count + 1000) {
 					mavlink_log_critical(&_mavlink_log_pub, "Accel %" PRIu8 " clipping, not safe to fly!\t", _instance);
 					/* EVENT
 					 * @description Land now, and check the vehicle setup.
@@ -419,8 +419,8 @@ bool VehicleIMU::UpdateAccel()
 					 */
 					events::send<uint8_t>(events::ID("vehicle_imu_accel_clipping"), events::Log::Critical,
 							      "Accel {1} clipping, not safe to fly!", _instance);
-					_last_clipping_notify_time = accel.timestamp_sample;
-					_last_clipping_notify_total_count = clipping_total;
+					_last_accel_clipping_notify_time = accel.timestamp_sample;
+					_last_accel_clipping_notify_total_count = clipping_total;
 				}
 			}
 		}
@@ -534,6 +534,52 @@ bool VehicleIMU::UpdateGyro()
 		_gyro_integrator.put(gyro_raw, dt);
 
 		updated = true;
+
+		if (gyro.clip_counter[0] > 0 || gyro.clip_counter[1] > 0 || gyro.clip_counter[2] > 0) {
+			// rotate sensor clip counts into vehicle body frame
+			const Vector3f clipping{_gyro_calibration.rotation() *
+						Vector3f{(float)gyro.clip_counter[0], (float)gyro.clip_counter[1], (float)gyro.clip_counter[2]}};
+
+			// round to get reasonble clip counts per axis (after board rotation)
+			const uint8_t clip_x = roundf(fabsf(clipping(0)));
+			const uint8_t clip_y = roundf(fabsf(clipping(1)));
+			const uint8_t clip_z = roundf(fabsf(clipping(2)));
+
+			_status.gyro_clipping[0] += clip_x;
+			_status.gyro_clipping[1] += clip_y;
+			_status.gyro_clipping[2] += clip_z;
+
+			if (clip_x > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::CLIPPING_X;
+			}
+
+			if (clip_y > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::CLIPPING_Y;
+			}
+
+			if (clip_z > 0) {
+				_delta_angle_clipping |= vehicle_imu_s::CLIPPING_Z;
+			}
+
+			_publish_status = true;
+
+			if (_gyro_calibration.enabled() && (hrt_elapsed_time(&_last_gyro_clipping_notify_time) > 3_s)) {
+				// start notifying the user periodically if there's significant continuous clipping
+				const uint64_t clipping_total = _status.gyro_clipping[0] + _status.gyro_clipping[1] + _status.gyro_clipping[2];
+
+				if (clipping_total > _last_gyro_clipping_notify_total_count + 1000) {
+					mavlink_log_critical(&_mavlink_log_pub, "Gyro %" PRIu8 " clipping, not safe to fly!\t", _instance);
+					/* EVENT
+					 * @description Land now, and check the vehicle setup.
+					 * Clipping can lead to fly-aways.
+					 */
+					events::send<uint8_t>(events::ID("vehicle_imu_gyro_clipping"), events::Log::Critical,
+							      "Gyro {1} clipping, not safe to fly!", _instance);
+					_last_gyro_clipping_notify_time = gyro.timestamp_sample;
+					_last_gyro_clipping_notify_total_count = clipping_total;
+				}
+			}
+		}
 	}
 
 	return updated;

src/modules/sensors/vehicle_imu/VehicleIMU.hpp
@@ -148,10 +148,15 @@ private:
 	float _coning_norm_accum{0};
 	float _coning_norm_accum_total_time_s{0};
 
-	uint8_t _delta_velocity_clipping{0};
+	uint8_t     _delta_angle_clipping{0};
+	uint8_t     _delta_velocity_clipping{0};
 
-	hrt_abstime _last_clipping_notify_time{0};
-	uint64_t _last_clipping_notify_total_count{0};
+	hrt_abstime _last_accel_clipping_notify_time{0};
+	hrt_abstime _last_gyro_clipping_notify_time{0};
+
+	uint64_t    _last_accel_clipping_notify_total_count{0};
+	uint64_t    _last_gyro_clipping_notify_total_count{0};
+
 	orb_advert_t _mavlink_log_pub{nullptr};
 
 	uint32_t _backup_schedule_timeout_us{20000};