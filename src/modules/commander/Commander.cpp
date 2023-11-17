zlowell2/PX4-Autopilot

ManualControl: introduce stick gesture for killing

src/modules/commander/Commander.cpp
@@ -2463,8 +2463,8 @@ Commander::run()
 					break;
 
 				case action_request_s::ACTION_UNKILL:
-					if (arm_disarm_reason == arm_disarm_reason_t::rc_switch && _armed.manual_lockdown) {
-						mavlink_log_info(&_mavlink_log_pub, "Kill-switch disengaged\t");
+					if (_armed.manual_lockdown) {
+						mavlink_log_info(&_mavlink_log_pub, "Kill disengaged\t");
 						events::send(events::ID("commander_kill_sw_disengaged"), events::Log::Info, "Kill-switch disengaged");
 						_status_changed = true;
 						_armed.manual_lockdown = false;
@@ -2473,8 +2473,8 @@ Commander::run()
 					break;
 
 				case action_request_s::ACTION_KILL:
-					if (arm_disarm_reason == arm_disarm_reason_t::rc_switch && !_armed.manual_lockdown) {
-						const char kill_switch_string[] = "Kill-switch engaged\t";
+					if (!_armed.manual_lockdown) {
+						const char kill_switch_string[] = "Kill engaged\t";
 						events::LogLevels log_levels{events::Log::Info};
 
 						if (_land_detector.landed) {

gau914sb/PX4-Voronoi

src/modules/commander/Commander.cpp
@@ -1615,9 +1615,9 @@ void Commander::executeActionRequest(const action_request_s &action_request)
 		break;
 
 	case action_request_s::ACTION_UNKILL:
-		if (arm_disarm_reason == arm_disarm_reason_t::rc_switch && _actuator_armed.manual_lockdown) {
-			mavlink_log_info(&_mavlink_log_pub, "Kill-switch disengaged\t");
-			events::send(events::ID("commander_kill_sw_disengaged"), events::Log::Info, "Kill-switch disengaged");
+		if (_actuator_armed.manual_lockdown) {
+			mavlink_log_info(&_mavlink_log_pub, "Kill disengaged\t");
+			events::send(events::ID("commander_kill_sw_disengaged"), events::Log::Info, "Kill disengaged");
 			_status_changed = true;
 			_actuator_armed.manual_lockdown = false;
 		}
@@ -1625,8 +1625,8 @@ void Commander::executeActionRequest(const action_request_s &action_request)
 		break;
 
 	case action_request_s::ACTION_KILL:
-		if (arm_disarm_reason == arm_disarm_reason_t::rc_switch && !_actuator_armed.manual_lockdown) {
-			const char kill_switch_string[] = "Kill-switch engaged\t";
+		if (!_actuator_armed.manual_lockdown) {
+			const char kill_switch_string[] = "Kill engaged\t";
 			events::LogLevels log_levels{events::Log::Info};
 
 			if (_vehicle_land_detected.landed) {
@@ -1637,7 +1637,7 @@ void Commander::executeActionRequest(const action_request_s &action_request)
 				log_levels.external = events::Log::Critical;
 			}
 
-			events::send(events::ID("commander_kill_sw_engaged"), log_levels, "Kill-switch engaged");
+			events::send(events::ID("commander_kill_sw_engaged"), log_levels, "Kill engaged");
 
 			_status_changed = true;
 			_actuator_armed.manual_lockdown = true;