Airial-UAS/PX4-Autopilot

MCP9808: Create driver for MCP9808 temperature sensor

src/drivers/drv_sensor.h
@@ -249,6 +249,8 @@
 #define DRV_BARO_DEVTYPE_AUAV		0xE7
 #define DRV_BARO_DEVTYPE_SPA06          0xE8
 
+#define DRV_TEMP_DEVTYPE_MCP9808	0xE8
+
 #define DRV_DEVTYPE_UNUSED		0xff
 
 #endif /* _DRV_SENSOR_H */

src/modules/logger/logged_topics.cpp
@@ -118,6 +118,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("sensor_combined");
 	add_optional_topic("sensor_correction");
 	add_optional_topic("sensor_gyro_fft", 50);
+	add_optional_topic("sensor_mcp9808", 10);
 	add_topic("sensor_selection");
 	add_topic("sensors_status_imu", 200);
 	add_optional_topic("spoilers_setpoint", 1000);

gau914sb/PX4-Voronoi

src/drivers/drv_sensor.h
@@ -262,6 +262,8 @@
 
 #define DRV_ADC_DEVTYPE_ADS7953 0xED
 
+#define DRV_TEMP_DEVTYPE_MCP9808	0xEE
+
 #define DRV_DEVTYPE_UNUSED		0xff
 
 #endif /* _DRV_SENSOR_H */

src/modules/logger/logged_topics.cpp
@@ -123,6 +123,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic("sensor_gyro_fft", 50);
 	add_topic("sensor_selection");
 	add_topic("sensors_status_imu", 200);
+	add_optional_topic("sensor_temp", 10);
 	add_optional_topic("spoilers_setpoint", 1000);
 	add_topic("system_power", 500);
 	add_optional_topic("takeoff_status", 1000);